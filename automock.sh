2698da802a57509e0e9a86c3de9909311c9192e7
Igor Gnatenko <i.gnatenko.brain@gmail.com>
2013-05-07 19:16:12 +0400

    i386 to i686 and comment createrepo in clean

--- automock.sh
+++ automock.sh
@@ -4,21 +4,21 @@ function buildrpm
 {
   #Build RPMs for x86_64
   mock -r fedora-$FEDVER-x86_64 --rebuild --resultdir=$HOMEDIR/repo/"%(dist)s"/"%(target_arch)s"/os/ $1
-  #Build RPMs for i386
-  mock -r fedora-$FEDVER-i386 --rebuild --resultdir=$HOMEDIR/repo/"%(dist)s"/"%(target_arch)s"/os/ $1
+  #Build RPMs for i686
+  mock -r fedora-$FEDVER-i686 --rebuild --resultdir=$HOMEDIR/repo/"%(dist)s"/"%(target_arch)s"/os/ $1
 }
 if [[ $1 = "clean" ]]; then
   rm -rf $HOMEDIR/repo/
-  mkdir $HOMEDIR/repo/ $HOMEDIR/repo/lastlogs/ $HOMEDIR/repo/lastlogs/source/ $HOMEDIR/repo/lastlogs/x86_64/ $HOMEDIR/repo/lastlogs/i386/
+  mkdir $HOMEDIR/repo/ $HOMEDIR/repo/lastlogs/ $HOMEDIR/repo/lastlogs/source/ $HOMEDIR/repo/lastlogs/x86_64/ $HOMEDIR/repo/lastlogs/i686/
   for (( ver=18 ; ver<=19 ; ver++ ))
     do
-      mkdir $HOMEDIR/repo/fc$ver/ $HOMEDIR/repo/fc$ver/x86_64/ $HOMEDIR/repo/fc$ver/i386/ $HOMEDIR/repo/fc$ver/source
-      mkdir $HOMEDIR/repo/fc$ver/x86_64/os/ $HOMEDIR/repo/fc$ver/i386/os/ $HOMEDIR/repo/fc$ver/x86_64/debug/ $HOMEDIR/repo/fc$ver/i386/debug/
-      createrepo $HOMEDIR/repo/fc$ver/source/
-      createrepo $HOMEDIR/repo/fc$ver/x86_64/os/
-      createrepo $HOMEDIR/repo/fc$ver/i386/os/
-      createrepo $HOMEDIR/repo/fc$ver/x86_64/debug/
-      createrepo $HOMEDIR/repo/fc$ver/i386/debug/
+      mkdir $HOMEDIR/repo/fc$ver/ $HOMEDIR/repo/fc$ver/x86_64/ $HOMEDIR/repo/fc$ver/i686/ $HOMEDIR/repo/fc$ver/source
+      mkdir $HOMEDIR/repo/fc$ver/x86_64/os/ $HOMEDIR/repo/fc$ver/i686/os/ $HOMEDIR/repo/fc$ver/x86_64/debug/ $HOMEDIR/repo/fc$ver/i686/debug/
+#      createrepo $HOMEDIR/repo/fc$ver/source/
+#      createrepo $HOMEDIR/repo/fc$ver/x86_64/os/
+#      createrepo $HOMEDIR/repo/fc$ver/i686/os/
+#      createrepo $HOMEDIR/repo/fc$ver/x86_64/debug/
+#      createrepo $HOMEDIR/repo/fc$ver/i686/debug/
     done
   exit
 elif [[ $1 = *.spec && $2 = 1[89] ]]; then
@@ -34,21 +34,21 @@ elif [[ $1 = *.spec && $2 = 1[89] ]]; then
   find $HOMEDIR/repo/fc$FEDVER/source/ -type f -name '*.log' -exec mv {} $HOMEDIR/repo/lastlogs/source/ \;
   #Move last x86_64 logs to separate directory
   find $HOMEDIR/repo/fc$FEDVER/x86_64/os/ -type f -name '*.log' -exec mv {} $HOMEDIR/repo/lastlogs/x86_64/ \;
-  #Move last i386 logs to separate directory
-  find $HOMEDIR/repo/fc$FEDVER/i386/os/ -type f -name '*.log' -exec mv {} $HOMEDIR/repo/lastlogs/i386/ \;
+  #Move last i686 logs to separate directory
+  find $HOMEDIR/repo/fc$FEDVER/i686/os/ -type f -name '*.log' -exec mv {} $HOMEDIR/repo/lastlogs/i686/ \;
   #Delete SRPMs from x86_64 non-source repo
   find $HOMEDIR/repo/fc$FEDVER/x86_64/os/ -type f -name '*.src.rpm' -delete
-  #Delete SRPMs from i386 non-source repo
-  find $HOMEDIR/repo/fc$FEDVER/i386/os/ -type f -name '*.src.rpm' -delete
+  #Delete SRPMs from i686 non-source repo
+  find $HOMEDIR/repo/fc$FEDVER/i686/os/ -type f -name '*.src.rpm' -delete
   #Delete temp mock files
   find $HOMEDIR/repo/fc$FEDVER/ -type f -not -name '*.rpm' -delete
   #Move debuginfo x86_64 to separate repository
   find $HOMEDIR/repo/fc$FEDVER/x86_64/os/ -type f -name '*debuginfo*' -exec mv {} $HOMEDIR/repo/fc$FEDVER/x86_64/debug/ \;
-  #Move debuginfo i386 to separate repository
-  find $HOMEDIR/repo/fc$FEDVER/i386/os/ -type f -name '*debuginfo*' -exec mv {} $HOMEDIR/repo/fc$FEDVER/x86_64/debug/ \;
+  #Move debuginfo i686 to separate repository
+  find $HOMEDIR/repo/fc$FEDVER/i686/os/ -type f -name '*debuginfo*' -exec mv {} $HOMEDIR/repo/fc$FEDVER/x86_64/debug/ \;
   createrepo --update $HOMEDIR/repo/fc$FEDVER/source/
   createrepo --update $HOMEDIR/repo/fc$FEDVER/x86_64/os/
-  createrepo --update $HOMEDIR/repo/fc$FEDVER/i386/os/
+  createrepo --update $HOMEDIR/repo/fc$FEDVER/i686/os/
   createrepo --update $HOMEDIR/repo/fc$FEDVER/x86_64/debug/
-  createrepo --update $HOMEDIR/repo/fc$FEDVER/i386/debug/
+  createrepo --update $HOMEDIR/repo/fc$FEDVER/i686/debug/
 fi
