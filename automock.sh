527180d674a0d8584bcde845029302e0586d00e5
Igor Gnatenko <i.gnatenko.brain@gmail.com>
2013-05-08 13:14:05 +0400

    fix #1

--- automock.sh
+++ automock.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 REPODIR="/home/`whoami`/repo"
-function buildrpm
-{
-  #Build RPMs for x86_64
-  mock -r fedora-$FEDVER-x86_64 --rebuild --resultdir=$REPODIR/"%(dist)s"/x86_64/os/ $1
-  #Build RPMs for i386
-  mock -r fedora-$FEDVER-i386 --rebuild --resultdir=$REPODIR/"%(dist)s"/i386/os/ $1
-}
 if [[ $1 = "clean" ]]; then
   rm -rf $REPODIR/
-  mkdir $REPODIR/ $REPODIR/lastlogs/ $REPODIR/lastlogs/source/ $REPODIR/lastlogs/x86_64/ $REPODIR/lastlogs/i386/
+  mkdir $REPODIR/
   for (( ver=18 ; ver<=19 ; ver++ ))
     do
       mkdir $REPODIR/fc$ver/ $REPODIR/fc$ver/x86_64/ $REPODIR/fc$ver/i386/ $REPODIR/fc$ver/source
-      mkdir $REPODIR/fc$ver/x86_64/os/ $REPODIR/fc$ver/i386/os/ $REPODIR/fc$ver/x86_64/debug/ $REPODIR/fc$ver/i386/debug/
     done
   exit
 elif [[ $1 = *.spec && $2 = 1[89] ]]; then
@@ -22,31 +14,32 @@ elif [[ $1 = *.spec && $2 = 1[89] ]]; then
   FEDVER="$2"
   PACKAGENAME=`basename $FILE | sed -e 's/\.spec//'`
   PACKAGEDIR=`dirname $FILE`
-  #Remove older RPMs, SRPM of this package
-  find $REPODIR/fc$FEDVER/ -type f -name '$PACKAGENAME*' -delete
+  #Remove older SRPMs, RPMs and logs
+  rm -rf $REPODIR/fc$FEDVER/source/$PACKAGENAME/ $REPODIR/fc$FEDVER/x86_64/$PACKAGENAME/ $REPODIR/fc$FEDVER/i386/$PACKAGENAME/
+  #Create logs dirs
+  mkdir -p $REPODIR/fc$FEDVER/source/$PACKAGENAME/logs/ $REPODIR/fc$FEDVER/x86_64/$PACKAGENAME/logs/ $REPODIR/fc$FEDVER/i386/$PACKAGENAME/logs/
   #Build SRPM
-  mock --buildsrpm --resultdir=$REPODIR/"%(dist)s"/source/ --spec $FILE --source $PACKAGEDIR/SOURCES/
-  #Call func to build RPMs
-  buildrpm $REPODIR/fc$FEDVER/source/$PACKAGENAME*.src.rpm
-  #Move last source logs to separate directory
-  find $REPODIR/fc$FEDVER/source/ -type f -name '*.log' -exec mv {} $REPODIR/lastlogs/source/ \;
-  #Move last x86_64 logs to separate directory
-  find $REPODIR/fc$FEDVER/x86_64/os/ -type f -name '*.log' -exec mv {} $REPODIR/lastlogs/x86_64/ \;
-  #Move last i386 logs to separate directory
-  find $REPODIR/fc$FEDVER/i386/os/ -type f -name '*.log' -exec mv {} $REPODIR/lastlogs/i386/ \;
-  #Delete SRPMs from x86_64 non-source repo
-  find $REPODIR/fc$FEDVER/x86_64/os/ -type f -name '*.src.rpm' -delete
-  #Delete SRPMs from i386 non-source repo
-  find $REPODIR/fc$FEDVER/i386/os/ -type f -name '*.src.rpm' -delete
-  #Delete temp mock files
-  find $REPODIR/fc$FEDVER/ -type f -not -name '*.rpm' -delete
-  #Move debuginfo x86_64 to separate repository
-  find $REPODIR/fc$FEDVER/x86_64/os/ -type f -name '*debuginfo*' -exec mv {} $REPODIR/fc$FEDVER/x86_64/debug/ \;
-  #Move debuginfo i386 to separate repository
-  find $REPODIR/fc$FEDVER/i386/os/ -type f -name '*debuginfo*' -exec mv {} $REPODIR/fc$FEDVER/i386/debug/ \;
+  mock --buildsrpm --resultdir=$REPODIR/"%(dist)s"/source/$PACKAGENAME/ --spec $FILE --source $PACKAGEDIR/SOURCES/
+  #Move source logs to separate directory
+  find $REPODIR/fc$FEDVER/source/$PACKAGENAME/ -type f -name '*.log' -exec mv -f {} $REPODIR/fc$FEDVER/source/$PACKAGENAME/logs/ \;
+  #Delete temp mock files and SRPMs from source repo
+  find $REPODIR/fc$FEDVER/source/$PACKAGENAME/ -type f -regextype "posix-extended" -not -regex '.*\.(rpm|log)' -delete
+  #Update source repo
   createrepo --update $REPODIR/fc$FEDVER/source/
-  createrepo --update $REPODIR/fc$FEDVER/x86_64/os/
-  createrepo --update $REPODIR/fc$FEDVER/i386/os/
-  createrepo --update $REPODIR/fc$FEDVER/x86_64/debug/
-  createrepo --update $REPODIR/fc$FEDVER/i386/debug/
+  #Build RPMs for x86_64
+  mock -r fedora-$FEDVER-x86_64 --rebuild --resultdir=$REPODIR/"%(dist)s"/x86_64/$PACKAGENAME/ $REPODIR/fc$FEDVER/source/$PACKAGENAME/*.src.rpm
+  #Move x86_64 logs to separate directory
+  find $REPODIR/fc$FEDVER/x86_64/$PACKAGENAME/ -type f -name '*.log' -exec mv -f {} $REPODIR/fc$FEDVER/x86_64/$PACKAGENAME/logs/ \;
+  #Delete temp mock files and SRPMs from x86_64 repo
+  find $REPODIR/fc$FEDVER/x86_64/$PACKAGENAME/ -type f -regextype "posix-extended" -not -regex '.*\.(rpm|log)' -o -name '*.src.rpm' -delete
+  #Update x86_64 repo
+  createrepo --update $REPODIR/fc$FEDVER/x86_64/
+  #Build RPMs for i386
+  mock -r fedora-$FEDVER-i386 --rebuild --resultdir=$REPODIR/"%(dist)s"/i386/$PACKAGENAME/ $REPODIR/fc$FEDVER/source/$PACKAGENAME/*.src.rpm
+  #Move i386 logs to separate directory
+  find $REPODIR/fc$FEDVER/i386/$PACKAGENAME/ -type f -name '*.log' -exec mv -f {} $REPODIR/fc$FEDVER/i386/$PACKAGENAME/logs/ \;
+  #Delete temp mock files and SRPMs from i386 repo
+  find $REPODIR/fc$FEDVER/i386/$PACKAGENAME/ -type f -regextype "posix-extended" -not -regex '.*\.(rpm|log)' -o -name '*.src.rpm' -delete
+  #Update i386 repo
+  createrepo --update $REPODIR/fc$FEDVER/i386/
 fi
