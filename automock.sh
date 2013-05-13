2a03c0230b4bca6c05a47c140a1bcc83e0876bcd
Igor Gnatenko <i.gnatenko.brain@gmail.com>
2013-05-14 01:39:52 +0400

    fix code (permissions)

--- automock.sh
+++ automock.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-REPODIR="/home/`whoami`/repo"
+REPODIR="/home/repos"
 function updaterepo
 {
   createrepo --update $REPODIR/fc$FEDVER/$1/
@@ -14,8 +14,8 @@ function build_clean
   updaterepo $1
 }
 if [[ $1 = clean ]]; then
-  rm -rf $REPODIR/
-  mkdir $REPODIR/
+  rm -rf $REPODIR/*
+#  mkdir $REPODIR/
 elif [[ $1 = *.spec && $2 = 1[89] ]]; then
   FILE=`readlink -f $1`
   FEDVER="$2"
@@ -23,6 +23,8 @@ elif [[ $1 = *.spec && $2 = 1[89] ]]; then
   PACKAGEDIR=`dirname $FILE`
   #Remove older SRPMs and RPMs
   rm -rf $REPODIR/fc$FEDVER/source/$PACKAGENAME/ $REPODIR/fc$FEDVER/x86_64/$PACKAGENAME/ $REPODIR/fc$FEDVER/i386/$PACKAGENAME/
+  #Create dirs
+  mkdir -p $REPODIR/fc$FEDVER/source/$PACKAGENAME/ $REPODIR/fc$FEDVER/x86_64/$PACKAGENAME/ $REPODIR/fc$FEDVER/i386/$PACKAGENAME/
   #Build SRPM
   mock --buildsrpm --resultdir=$REPODIR/"%(dist)s"/source/$PACKAGENAME/ --spec $FILE --source $PACKAGEDIR/SOURCES/
   #Delete temp mock files and SRPMs from source repo
