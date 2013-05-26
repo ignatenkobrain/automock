7fcb32f6f3c178f55508d4ecbc083895253fe5da
Igor Gnatenko <i.gnatenko.brain@gmail.com>
2013-05-26 21:46:34 +0400

    update all variable in bash syntax

--- automock.sh
+++ automock.sh
@@ -15,48 +15,48 @@ function updateselinux
 function build_clean
 {
   # Build RPMs for x86_64
-  mock -r fedora-$FEDVER-$1 --arch $2 --rebuild --resultdir=$REPO/fc$FEDVER/$1/$2/ $REPO/fc$FEDVER/source/*.src.rpm
-  # Delete temp mock files and SRPMs from $1 repo
-  find $REPO/fc$FEDVER/$1/$2 -type f -regextype "posix-extended" -not -regex '.*\.(rpm|log)' -o -name '*.src.rpm' | xargs rm -f
+  mock -r fedora-${FEDVER}-${1} --arch ${2} --rebuild --resultdir=${REPO}/fc${FEDVER}/${1}/${2}/ ${REPO}/fc${FEDVER}/source/*.src.rpm
+  # Delete temp mock files and SRPMs from ${1} repo
+  find ${REPO}/fc${FEDVER}/${1}/${2} -type f -regextype "posix-extended" -not -regex '.*\.(rpm|log)' -o -name '*.src.rpm' | xargs rm -f
   updateselinux
 }
-if [[ $1 =~ ^git://.*\.git\?#[a-z0-9]{40}$ && $2 = 1[89] ]]; then
+if [[ ${1} =~ ^git://.*\.git\?#[a-z0-9]{40}$ && ${2} = 1[89] ]]; then
   # Cutting reponame
   REPONAME="${1##git:*/}"
   REPONAME="${REPONAME%.*}"
   # Cutting commit
   COMMIT="${1:(-40)}"
+  # Initializate version Fedora
+  FEDVER="${2}"
   # Initializate REPO variable at date
-  REPO="${REPODIR}/`date +"%d.%m.%Y-%H:%M:%S"`-$REPONAME"
+  REPO="${REPODIR}/`date +"%d.%m.%Y-%H:%M:%S"`-${REPONAME}-${FEDVER}"
   # Cloning git repo
   git clone ${1%?#*} ${REPO}
   # Initializate git dirs
   export GIT_WORK_TREE="${REPO}"
   export GIT_DIR="${GIT_WORK_TREE}/.git"
-  # Reset HEAD to sha in $2
+  # Reset HEAD to sha in ${2}
   git reset --hard ${COMMIT}
   # Read full link to spec file
   FILE=$(readlink -f ${REPO}/*.spec)
-  # Initializate version Fedora
-  FEDVER="$2"
   # Create src dir (temporary)
   mkdir -p ${REPO}/SOURCES/
   # Move sources to separate dir
-  find $REPO -maxdepth 1 -type f -regextype "posix-extended" -not -regex '.*\.spec|.*\/README.md' -exec mv -f {} $REPO/SOURCES/ \;
+  find ${REPO} -maxdepth 1 -type f -regextype "posix-extended" -not -regex '.*\.spec|.*\/README.md' -exec mv -f {} ${REPO}/SOURCES/ \;
   # Build SRPM
-  mock -r fedora-$FEDVER-`arch` --buildsrpm --resultdir=$REPO/fc$FEDVER/source/ --spec $FILE --source $REPO/SOURCES/
+  mock -r fedora-${FEDVER}-`arch` --buildsrpm --resultdir=${REPO}/fc${FEDVER}/source/ --spec ${FILE} --source ${REPO}/SOURCES/
   # Move sources from separate dir
-  mv $REPO/SOURCES/* $REPO/
+  mv ${REPO}/SOURCES/* ${REPO}/
   # Remove temp separate dir for sources
-  rm -rf $REPO/SOURCES/
+  rm -rf ${REPO}/SOURCES/
   # Delete temp mock files and SRPMs from source repo
-  find $REPO/fc$FEDVER/source/ -type f -regextype "posix-extended" -not -regex '.*\.(rpm|log)' -delete
+  find ${REPO}/fc${FEDVER}/source/ -type f -regextype "posix-extended" -not -regex '.*\.(rpm|log)' -delete
   updateselinux
   build_clean "x86_64" "x86_64"
   build_clean "x86_64" "i386"
   build_clean "i386" "i386"
-elif [[ $1 = clean ]]; then
-  rm -rf $REPODIR/*
-elif [[ $1 = update ]]; then
+elif [[ ${1} = clean ]]; then
+  rm -rf ${REPODIR}/*
+elif [[ ${1} = update ]]; then
   updateselinux
 fi
