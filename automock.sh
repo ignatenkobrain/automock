0b11fdba8cddfa2a5dd1ad5e93ec32cb86402ade
Igor Gnatenko <i.gnatenko.brain@gmail.com>
2013-05-27 00:16:05 +0400

    update readme, fix sudoers and automock for new user

--- automock.sh
+++ automock.sh
@@ -2,8 +2,8 @@
 REPODIR="/home/repos/build"
 function updateselinux
 {
-  SELINUXSTATUS=`$(which sestatus) | grep "SELinux status" | awk '{print($3)}'`
-  SELINUXHOMEDIRSSTATUS=`$(which getsebool) httpd_enable_homedirs | awk '{print($3)}'`
+  SELINUXSTATUS=`sestatus | grep "SELinux status" | awk '{print($3)}'`
+  SELINUXHOMEDIRSSTATUS=`getsebool httpd_enable_homedirs | awk '{print($3)}'`
   if [[ $SELINUXSTATUS = enabled ]]; then
     if [[ $REPODIR = /home/* && $SELINUXHOMEDIRSSTATUS = off ]]; then
       sudo setsebool -P httpd_enable_homedirs 1
