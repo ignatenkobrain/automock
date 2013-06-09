98d8f978ca5fa1ca254293e9822a9f188871e00c
Igor Gnatenko <i.gnatenko.brain@gmail.com>
2013-06-10 01:30:32 +0400

    update of derlaft

--- web/js/automock.js
+++ web/js/automock.js
@@ -1,11 +1,39 @@
 $(document).ready(function() {
+
+    var key = "";
+    var test = null;
+
+    readkey(document.getElementById('rsa_key').files[0]);
+
     if (window.File && window.FileReader) {
         // we have file api and js
-        $('#err_api').hide(); //hide message
-        $('#btn_submit').removeAttr('disabled'); //enable submit button
+        $('#err_api').hide();
+        $('#btn_submit').removeAttr('disabled');
     }
 
     $('#btn_submit').click(function() {
-        submit();
+        $('#err_read').hide();
+
+        if (key) {
+            alert(key);
+        }
+        else {
+            $('#err_read').show(100);
+        }
     })
+
+    function readkey(keyfile) {
+        var reader = new FileReader();
+        if (keyfile) {
+            reader.onload = function(red) {
+                key = red.target.result;
+            }
+            reader.readAsText(keyfile); 
+        }
+    }
+
+    $('#rsa_key').on("change ready", function(selected) {
+        var file = selected.target.files[0];
+        readkey(file);
+    });
 });
