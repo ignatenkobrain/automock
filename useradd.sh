0e63f21c44b803a33d42822b8e2e76564cfc9ebc
Igor Gnatenko <i.gnatenko.brain@gmail.com>
2013-06-09 15:09:16 +0400

    fix create ssh dir

--- useradd.sh
+++ useradd.sh
@@ -2,7 +2,7 @@
 if [[ `whoami` = root ]]; then
   USER="automock"
   useradd -G nginx,mock -s /bin/bash ${USER}
-  mkdir /home/${USER}/.ssh/
+  mkdir -p /home/${USER}/.ssh/
   touch /home/${USER}/.ssh/authorized_keys
   chown -R ${USER}:${USER} /home/build/.ssh/
   chmod 700 /home/${USER}/.ssh/
