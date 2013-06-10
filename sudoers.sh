26bfce82e03b6aee567903d8640b73cac1ed11d3
Igor Gnatenko <i.gnatenko.brain@gmail.com>
2013-06-10 11:53:33 +0400

    small fixes

--- sudoers.sh
+++ sudoers.sh
@@ -3,7 +3,8 @@ source automock.conf
 if [[ `whoami` = root ]]; then
   cp /etc/sudoers /home/${USER}/sudoers
   echo "Defaults:automock !requiretty" >> /etc/sudoers
-  echo "automock ALL=(ALL) NOPASSWD: /usr/sbin/semanage, /usr/sbin/restorecon, /usr/sbin/setsebool, /usr/bin/rm, /usr/bin/chown" >> /etc/sudoers
+  echo "${USER} ALL=(${USER}) NOPASSWD: /usr/sbin/semanage, /usr/sbin/restorecon, /usr/sbin/setsebool, /usr/bin/rm, /usr/bin/chown" >> /etc/sudoers
+  echo "apache ALL=(${USER}) NOPASSWD: ${AUTOMOCK}/automock.sh" >> /etc/sudoers
   exit 0
 else
   echo "Failed! Run as root!"
